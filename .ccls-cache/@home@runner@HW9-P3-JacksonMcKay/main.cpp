76a03f83284115c5ce1cccedc97b0def8b960680
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-10-23 22:26:45 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW9-P3-JacksonMcKay/main.cpp
@@ -0,0 +1,12 @@
+//Title: HW9 Problem 3, Bronson pg 458 #12
+//Description: Inputting answers to tests to get grades
+//Programmer: Jackson McKay
+//Course: CSCI130-001 Dr. Mellenthin
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+}
